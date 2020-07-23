7e285418c758d2d2d44c25375e2abaa7b5fa0f7f
Sandro Ferri <sandro_junior@hotmail.com>
2020-07-23 11:26:35 -0300

    Implementação da logica em js

--- js/confScript.js
+++ js/confScript.js
@@ -1,52 +1,18 @@
-var caracter = null
-var resultado = null
-var num1 = null
-var num2 = null
-function  seleciona(botao) {
-    caracter = botao
+function  calcular(tipo, valor) {
+    if(tipo === 'valor'){
+        document.getElementById('display').value += valor
 
-    switch (caracter) {
-        case 'C':
-            document.getElementById('display').value = ''
-            break
-        case '+':
-            resultado = parseFloat('caracter') + parseFloat('caracter')
-            break
-        case '-':
-            resultado = caracter - caracter
-            break
-        case '/':
-            resultado = caracter / caracter
-            break
-        case '*':
-            resultado = caracter * caracter
-            break
-        case '=':
-            document.getElementById('display').value = resultado
-            break
-        default:
-            resultado = caracter
-            document.getElementById('display').value += resultado
     }
-}
-
-function calular(simbolo) {
-
-    switch (simbolo) {
-        case '+':
-            resultado = parseFloat('caracter') + parseFloat('caracter')
-            break
-        case '-':
-            resultado = caracter - caracter
-            break
-        case '/':
-            resultado = caracter / caracter
-            break
-        case '*':
-            resultado = caracter * caracter
-            break
-        case '=':
+    if (tipo === 'acao'){
+        if(valor === 'C') {
+            document.getElementById('display').value = ''
+        }
+        if(valor === '+' || valor === '-' || valor === '/' || valor === '*' || valor === '.'){
+            document.getElementById('display').value += valor
+        }
+        if(valor === '='){
+            var resultado = eval(document.getElementById('display').value)
             document.getElementById('display').value = resultado
-            break
+        }
     }
 }
